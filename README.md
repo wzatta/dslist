62898610a0bf3dae12819a149b98d2f275b7702f
wzatta <wzjssa@gmail.com>
2023-05-09 22:10:33 -0300

    Properties, Game, ORM

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dslist
+Intenseivão Java Spring [08 a 12/05]
---
+++ src/main/java/com/cilazatta/dslist/entities/Game.java
@@ -0,0 +1,38 @@
+package com.cilazatta.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_game")
+public class Game {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String plataforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+
+}
