8d16a53a530a869a68bd6308b6bff7b200e0ec27
wzatta <wzjssa@gmail.com>
2023-05-09 22:28:41 -0300

    Game Seed

--- src/main/java/com/cilazatta/dslist/entities/Game.java
+++ src/main/java/com/cilazatta/dslist/entities/Game.java
@@ -28,10 +28,12 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 
